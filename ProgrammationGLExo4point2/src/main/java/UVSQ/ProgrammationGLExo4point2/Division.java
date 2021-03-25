ca179cb69a4bd0d38c63a0249b34d7a6f24d4814
Ait Slimane <rachid.a81@yahoo.com>
2021-03-25 23:39:39 +0100

    Creation du test testConstructeurDivision

--- ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/Division.java
+++ ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/Division.java
@@ -1,6 +1,10 @@
 package UVSQ.ProgrammationGLExo4point2;
 
-public class Division implements Command {
+public class Division extends AbstractCommandClass implements Command {
+	public Division(MoteurRPN rpn)
+	{
+		
+	}
 
 	public void execute() {
 		// TODO Auto-generated method stub
