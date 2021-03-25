457039ea8b163c34d960dca90e7af2d9e07aa511
Ait Slimane <rachid.a81@yahoo.com>
2021-03-25 23:30:55 +0100

    Creation du test TestConstructeurSoustraction

--- ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/Soustraction.java
+++ ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/Soustraction.java
@@ -1,7 +1,11 @@
 package UVSQ.ProgrammationGLExo4point2;
 
-public class Soustraction implements Command{
+public class Soustraction extends AbstractCommandClass implements Command{
 
+	public Soustraction(MoteurRPN rpn)
+	{
+		
+	}
 	public void execute() {
 		// TODO Auto-generated method stub
 		
