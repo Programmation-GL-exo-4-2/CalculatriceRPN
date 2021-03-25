398cdf31edf6ae227acfdc4e56ee2260d8f7ccb4
Ait Slimane <rachid.a81@yahoo.com>
2021-03-25 23:36:40 +0100

    Creation du test testConstructeurMultiplication

--- ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/Multiplication.java
+++ ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/Multiplication.java
@@ -1,7 +1,11 @@
 package UVSQ.ProgrammationGLExo4point2;
 
-public class Multiplication implements Command{
-
+public class Multiplication extends AbstractCommandClass implements Command{
+	
+	public Multiplication(MoteurRPN rpn)
+	{
+		
+	}
 	public void execute() {
 		// TODO Auto-generated method stub
 		
