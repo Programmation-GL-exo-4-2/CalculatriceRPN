6d4e8c9ff7d0ffed8b9dda37bea770833cceaedd
Ait Slimane <rachid.a81@yahoo.com>
2021-03-25 23:33:07 +0100

    Réussite du test TestConstructeurSoustraction

--- ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/Soustraction.java
+++ ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/Soustraction.java
@@ -4,7 +4,7 @@ public class Soustraction extends AbstractCommandClass implements Command{
 
 	public Soustraction(MoteurRPN rpn)
 	{
-		
+		this.setInter(rpn);
 	}
 	public void execute() {
 		// TODO Auto-generated method stub
