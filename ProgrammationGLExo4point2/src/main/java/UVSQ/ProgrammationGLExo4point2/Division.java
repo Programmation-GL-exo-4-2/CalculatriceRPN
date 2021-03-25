bb388472b4779b17855aa692585f41523c4e9892
Ait Slimane <rachid.a81@yahoo.com>
2021-03-25 23:40:03 +0100

    Réussite du test TestConstructeurDivision

--- ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/Division.java
+++ ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/Division.java
@@ -3,7 +3,7 @@ package UVSQ.ProgrammationGLExo4point2;
 public class Division extends AbstractCommandClass implements Command {
 	public Division(MoteurRPN rpn)
 	{
-		
+		this.setInter(rpn);
 	}
 
 	public void execute() {
