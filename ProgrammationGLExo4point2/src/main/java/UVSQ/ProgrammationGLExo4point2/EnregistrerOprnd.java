ecbce491f3cb5410518daed8fd997b9aae2a49c6
Ait Slimane <rachid.a81@yahoo.com>
2021-03-26 13:07:54 +0100

    Creation du test testConstructeur de la classe EnregistrerOprnd

---
+++ ProgrammationGLExo4point2/src/main/java/UVSQ/ProgrammationGLExo4point2/EnregistrerOprnd.java
@@ -0,0 +1,14 @@
+package UVSQ.ProgrammationGLExo4point2;
+
+public class EnregistrerOprnd extends AbstractCommandClass implements Command {
+
+	public EnregistrerOprnd(MoteurRPN rpn)
+	{
+		
+	}
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ ProgrammationGLExo4point2/src/test/java/UVSQ/ProgrammationGLExo4point2/EnregistrerOprndTest.java
@@ -0,0 +1,13 @@
+package UVSQ.ProgrammationGLExo4point2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EnregistrerOprndTest {
+
+	@Test
+	public void testConstructeurEng() {
+	}
+
+}
